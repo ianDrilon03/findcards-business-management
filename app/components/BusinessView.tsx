8d81abc65be551a180c8b7f720d86cf0e40b99b5
ianDrilon03 <ian@findfoundation.org.au>
2025-07-22 15:43:52 +0800

    updating view

--- app/components/BusinessView.tsx
+++ app/components/BusinessView.tsx
@@ -8,14 +8,20 @@ export function BusinessView(): JSX.Element {
       <CardHeader className='p-0'>
         <Image
           src='http://127.0.0.1:54321/storage/v1/object/public/business/6f526a43-fc13-408c-88f5-04d22db86d7b/New%20Business/pexels-apgpotr-683039.jpg'
-          width={500}
-          height={500}
+          width={1000}
+          height={1000}
+          quality={80}
           className='w-full h-[30rem] object-cover rounded-lg'
           alt='test'
         />
 
         <CardContent className='pb-4 pt-2'>
-          <CardTitle className='text-2xl'>Ocean View Restaurant</CardTitle>
+          <section>
+            <CardTitle className='text-2xl'>Ocean View Restaurant</CardTitle>
+            <span className='text-sm text-gray-500'>Health and Fitness</span>
+          </section>
+
+          <main></main>
         </CardContent>
       </CardHeader>
     </Card>
