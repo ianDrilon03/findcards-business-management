019dee0d5e3ed9057290d06463838c7dd24a2108
ianDrilon03 <ian@findfoundation.org.au>
2025-07-25 16:03:17 +0800

    Adding categories page

---
+++ app/backend/[userId]/category/page.tsx
@@ -0,0 +1,13 @@
+import { JSX } from 'react'
+import { Container } from '@/components/custom/Container'
+
+export default function CategoryPage(): JSX.Element {
+  return (
+    <Container
+      title='Manage Category'
+      description='You can manage categories here'
+    >
+      Categories
+    </Container>
+  )
+}
